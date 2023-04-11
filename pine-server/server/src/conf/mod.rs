31bcd1bd725514c11576d9af69c6d0917543f704
Javad Sabbagh <javad.sabbaq@gmail.com>
2023-04-11 23:45:33 +0330

    Add config module

---
+++ pine-server/server/src/conf/mod.rs
@@ -0,0 +1,16 @@
+
+
+// todo define config structure and methods here
+
+pub struct ServerConfig {
+    pub logger: Logger,
+    pub database: Database
+}
+
+pub struct Logger {
+
+}
+
+pub struct Database {
+
+}
